cac9aebb2471ee613ee0d71bfdbc50cfa4db642d
GraceWCH <miaomaiowu@gmail.com>
2024-07-14 02:56:34 +0000

    modified

--- linebot_test/main.py
+++ linebot_test/main.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "<h1>My first web site</h1>"
+
+@app.route("/pwd")
+def pwd():
+    return f'''
+    <h2>{os.environ["CHANNEL_ACCESS_TOKEN"]}</h2>
+    <h2>{os.environ["CHANNEL_SECRET"]}</h2>
+    <h2>{os.environ["Gemini_API_KEY"]}</h2>
+'''
